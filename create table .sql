a72ea4a47110a358454d38cdd7c7070d0db1340d
rosithkumar143 <37369882+rosithkumar143@users.noreply.github.com>
2018-04-30 11:40:25 +0530

    Create create table .sql

---
+++ create table .sql
@@ -0,0 +1,6 @@
+show databases;
+create databse kumar1;
+use kumar1;
+create table kumar1(roll_no varchar(10)not null,name varchar(20),fathername varchar(15),email varchar(15),
+                    qulification varchar(20),college varchar(30),city varchar(20),district varchar(15),state varchar(20),pincode varchar(10));
+desc kumar1;
