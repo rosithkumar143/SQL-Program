f74b54e06917036dbd7174dda5246b83081ef809
rosithkumar143 <37369882+rosithkumar143@users.noreply.github.com>
2018-04-30 11:41:21 +0530

    Create insert and select values.sql

---
+++ insert and select values.sql
@@ -0,0 +1,6 @@
+show databases;
+create database sample1;
+use sample1;
+create table sample1(firsr varchar(20),sec varchar(20),third varchar(20));
+insert into sample1 values("pink","yello","green",);
+select * from sample1; 
